9016eb497a99875ab1f4816cfd7fd5b560b7363f
Brendow271 <pashashkuropat@yandex.ru>
2025-09-30 19:12:44 +0300

    Create validate registration data logic

--- project/Harmony/backend/src/main/java/com/harmony/service/ServiceUserAuth.java
+++ project/Harmony/backend/src/main/java/com/harmony/service/ServiceUserAuth.java
@@ -17,5 +17,45 @@ public class ServiceUserAuth {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-   
+
+    
+
+    private void validateRegistrationData(String name, String email, String password){
+
+        if (name == null || name.trim().isEmpty()){
+            throw new RuntimeException("Имя не может быть пустым");
+        }
+
+        if (name.length() < 2 || name.length() > 35){
+            throw new RuntimeException("Имя должно содержать от 2 до 35 символов");
+        }
+
+        validateEmail(email);
+        validatePassword(password);
+    }
+
+    private void validateEmail(String email){
+
+        if (email == null || email.trim().isEmpty()){
+            throw new RuntimeException("Email не может быть пустым");
+        }
+
+        if (!email.matches("^[A-Za-z0-9+_.-]+@(.+)$")){
+            throw new RuntimeException("Некорректный формат email");
+        }
+
+        if (email.length() > 255) {
+            throw new RuntimeException("Email слишком длинный");
+        }
+    }
+
+    private void validatePassword(String password){
+        if (password == null || password.trim().isEmpty()){
+            throw new RuntimeException("Email не может быть пустым");
+        }
+
+        if (password.length() < 8 || password.length() > 35){
+            throw new RuntimeException("Пароль должен содержать от 8 до 35 символов");
+        }
+    }
 }
