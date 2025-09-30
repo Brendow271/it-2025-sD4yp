cba0a5e1e0090e73d544ee89f8f0ffacb10a44b3
Brendow271 <pashashkuropat@yandex.ru>
2025-09-30 16:44:35 +0300

    Created entity user_auth

---
+++ project/Harmony/backend/src/main/java/com/harmony/entity/UserAuth.java
@@ -0,0 +1,81 @@
+package com.harmony.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "user_auth")
+public class UserAuth {
+
+    @Id
+    @GeneratedValue
+    private Long user_id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "is_active")
+    private boolean isActive = true;
+
+    public UserAuth(){};
+
+    public UserAuth(String name, String passwordHash, String email)
+    {
+        this.name = name;
+        this.passwordHash = passwordHash;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
