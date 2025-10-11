6abacb0c0f74088333ccc7f579e2c9290aa99947
Сороколетов Денис <90652246+DENIS-SOROKOLETOV@users.noreply.github.com>
2025-10-12 02:47:03 +0300

    style header component

--- project/Harmony/frontend/app/components/Header.tsx
+++ project/Harmony/frontend/app/components/Header.tsx
@@ -2,9 +2,19 @@ import Link from 'next/link';
 
 const Header = () =>{
     return(
-        <header className = "grid grid-cols-3 items-center text-black bg-gray-300 px-8 h-20">
+        <header className = "grid grid-cols-3 items-center text-black bg-gray-300 px-8 h-20 mb-6">
             <div className="flex justify-start h-full"><Link href={'/'} className="flex items-center px-3 hover:bg-gray-400"><img className="w-8 h-8" src="/assets/settings.png" alt="Настройки"/></Link></div>
-            <div className="flex h-full justify-center text-2xl"><Link href={'/'} className="flex items-center px-3 hover:bg-gray-400">Harmony</Link></div>
+            <div className="flex h-full justify-center text-2xl">
+                <Link href={'/'} className="flex items-center px-3">
+                    <div className="hover:text-3xl">H</div>
+                    <div className="hover:text-3xl">A</div>
+                    <div className="hover:text-3xl">R</div>
+                    <div className="hover:text-3xl">M</div>
+                    <div className="hover:text-3xl">O</div>
+                    <div className="hover:text-3xl">N</div>
+                    <div className="hover:text-3xl">Y</div>
+                </Link>
+            </div>
             <ul className = "flex items-center justify-end font-semibold text-base h-full">
                 <li className="flex h-full items-center hover:bg-gray-400 px-3"><Link href={'/pages/chat'}><img src="/assets/chat.png" alt="Чат" className="h-8 w-8"/></Link></li>
                 <li className="flex h-full items-center hover:bg-gray-400 px-3"><Link href={'/pages/likes'}><img src="/assets/heart.png" alt="Лайк" className="h-8 w-8"/></Link></li>
