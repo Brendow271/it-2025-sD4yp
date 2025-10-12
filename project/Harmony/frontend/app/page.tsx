3de0b0174f69c3673b9fb1f84940e3e8bacf64a0
Сороколетов Денис <90652246+DENIS-SOROKOLETOV@users.noreply.github.com>
2025-10-12 03:26:47 +0300

    add new dependencies

--- project/Harmony/frontend/app/page.tsx
+++ project/Harmony/frontend/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import React, {useState} from "react";
 import {useAuth} from "./hooks/useAuth";
 
 export default function Home() {
--- project/Harmony/frontend/app/pages/likes/page.tsx
+++ project/Harmony/frontend/app/pages/likes/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 export default function Likes(){
     return(
         <h1>Likes</h1>
