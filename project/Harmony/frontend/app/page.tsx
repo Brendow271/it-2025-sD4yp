302b9944cc6bab56931971fd88b6ec890d9d848a
Сороколетов Денис <90652246+DENIS-SOROKOLETOV@users.noreply.github.com>
2025-10-12 03:26:03 +0300

    add logout button

--- project/Harmony/frontend/app/page.tsx
+++ project/Harmony/frontend/app/page.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+'use client'
+
+import {useAuth} from "./hooks/useAuth";
+
 export default function Home() {
+    const {logout, isAuthenticated} = useAuth();
+    if (isAuthenticated){
+        return(
+            <button onClick = {logout}>Выйти</button>
+        )
+    }
     return (
-        <h1>
-            Home
-        </h1>
+        <div>
+            <h1>
+                Home
+            </h1>
+
+        </div>
+
   );
 }
