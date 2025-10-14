10d449a02ad2f1392f67cf8068f3b82bb573c99f
Сороколетов Денис <90652246+DENIS-SOROKOLETOV@users.noreply.github.com>
2025-10-14 21:30:21 +0300

    add SwipeCards to Home page

--- project/Harmony/frontend/app/page.tsx
+++ project/Harmony/frontend/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, {useState} from "react";
 import {useAuth} from "./hooks/useAuth";
+import SwipeCards from "./components/SwipeCards";
 
 export default function Home() {
     const {logout, isAuthenticated} = useAuth();
@@ -14,6 +15,7 @@ export default function Home() {
         <div>
             <h1>
                 Home
+                <SwipeCards/>
             </h1>
 
         </div>
