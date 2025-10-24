5e8559f5cd132bf4787271cf70c132be343e340a
Brendow271 <pashashkuropat@yandex.ru>
2025-10-24 22:58:34 +0300

    Delete extra import

--- project/Harmony/backend/src/main/java/com/harmony/repository/InstrumentsRepository.java
+++ project/Harmony/backend/src/main/java/com/harmony/repository/InstrumentsRepository.java
@@ -4,8 +4,6 @@ import com.harmony.entity.Instruments;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface InstrumentsRepository extends JpaRepository<Instruments, String> {
 }
