3d94b0c8db12b319ddae20d4f1456b36fc7d4997
Сороколетов Денис <90652246+DENIS-SOROKOLETOV@users.noreply.github.com>
2025-11-29 03:35:04 +0300

    add isAuthentificated check

--- project/Harmony/frontend/app/pages/auth/page.tsx
+++ project/Harmony/frontend/app/pages/auth/page.tsx
@@ -2,9 +2,32 @@
 import RegistrationForm from "../../components/RegistrationForm";
 import React, {useState, useEffect} from 'react'
 import LoginForm from "../../components/LoginForm";
+import Profile from "../profile/page"
+import {authCookies} from "../../components/common/cookies";
 
 export default function Auth(){
     const [activeTab, setActiveTab] = useState<'login' | 'register'>('login');
+    const [isAuthentificated, setIsAuthentificated] = useState(false);
+
+    useEffect(() => {
+        const token = authCookies.getAuthToken();
+        setIsAuthentificated(!!token);
+    }, []);
+
+    useEffect(() => {
+        const checkAuth = () => {
+            const token = authCookies.getAuthToken();
+            setIsAuthentificated(!!token);
+        };
+
+        const interval = setInterval(checkAuth, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    if (isAuthentificated) {
+        return <Profile />
+    }
+
     return(
         <div className="flex justify-center h-full items-center">
             <div className="flex flex-col w-full max-w-md">
