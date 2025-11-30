78aacd5ff41fc8c459a1553da7055e9938811e52
Brendow271 <pashashkuropat@yandex.ru>
2025-11-30 19:31:18 +0300

    Update logic matches by userId

--- project/Harmony/backend/src/main/java/com/harmony/repository/SwipeRepository.java
+++ project/Harmony/backend/src/main/java/com/harmony/repository/SwipeRepository.java
@@ -1,12 +1,11 @@
 package com.harmony.repository;
 
 import com.harmony.entity.Swipe;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,6 +20,6 @@ public interface SwipeRepository extends JpaRepository<Swipe, Long> {
     @Query("SELECT s FROM Swipe s WHERE (s.userId1 = :userId1 AND s.userId2 = :userId2) OR (s.userId1 = :userId2 AND s.userId2 = :userId1)")
     Optional<Swipe> findSwipeBetweenUsers(@Param("userId1") Long userId1, @Param("userId2") Long userId2);
 
-    @Query("SELECT s FROM Swipe s WHERE ((s.userId1 = :userId1 AND s.userId2 = :userId2 AND s.decision1 = true AND s.decision2 = true) OR (s.userId1 = :userId2 AND s.userId2 = :userId1 AND s.decision1 = true AND s.decision2 = true))")
-    List<Swipe> findMatchesForUser(@Param("userId1") Long userId1);
+    @Query("SELECT s FROM Swipe s WHERE (s.userId1 = :userId OR s.userId2 = :userId) AND s.decision1 = true AND s.decision2 = true")
+    List<Swipe> findMatchesForUser(@Param("userId") Long userId);
 }
